f8fda2422ea2595a74b296d4c43147cf8680206a
mrat <78581772+mr4tt@users.noreply.github.com>
2025-05-13 00:56:44 -0700

    feature: sending emails if correct file is found

---
+++ src/main.rs
@@ -0,0 +1,64 @@
+use lettre::message::header::ContentType;
+use lettre::transport::smtp::authentication::Credentials;
+use lettre::{Message, SmtpTransport, Transport};
+
+use chrono::{NaiveDate, Local};
+
+use dotenv;
+
+use std::fs;
+use std::env;
+
+fn main() {
+    let curr_path = env::current_exe().unwrap().display().to_string() + "/../../../";
+    let paths = fs::read_dir(format!("{curr_path}/letters")).unwrap();
+
+    // TODO: write a json that ties filenames to metadata (ex: date to send, date written, potentially photos)
+
+    // for each file, check if it matches today's date; send email if so
+    for path in paths {
+        let curr_date = Local::now().date_naive(); 
+
+        let file_name = path.unwrap().file_name().to_str().unwrap().to_string();
+        let send_date = NaiveDate::parse_from_str(file_name.split(".").next().unwrap(), "%Y-%m-%d");
+
+        match send_date {
+            Ok(_) => {
+                if send_date.unwrap() == curr_date {
+                    println!("found a match!");
+                    send_email(format!("{curr_path}letters/{file_name}"), &curr_path);
+                }
+            }
+            Err(_) => { continue; }
+        };
+    }
+}
+
+fn send_email(file_name: String, env_path: &str) {
+    dotenv::from_path(format!("{env_path}/.env")).ok();
+
+    let address = std::env::var("EMAIL").expect("Email address must be set");
+    let password = std::env::var("PASSWORD").expect("Email password must be set");
+
+    let email_body = fs::read_to_string(file_name).expect("Unable to read file");
+
+    let email = Message::builder()
+        .from(format!("Timey <{address}>").parse().unwrap())
+        .to(format!("Timey <{address}>").parse().unwrap())
+        .subject("Unearthed Time Capsule")
+        .header(ContentType::TEXT_PLAIN)
+        .body(email_body)
+        .unwrap();
+    
+    // Open a remote connection to gmail
+    let mailer = SmtpTransport::relay("smtp.gmail.com")
+        .unwrap()
+        .credentials(Credentials::new(address, password))
+        .build();
+
+    // Send the email
+    match mailer.send(&email) {
+        Ok(_) => println!("Email sent successfully!"),
+        Err(e) => panic!("Could not send email: {e:?}"),
+    }
+}
